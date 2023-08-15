46129e66edf3680b2dd663bd39b1fb1dd6a4b0a3
Zahid295 <>
2023-08-15 23:37:30 +0000

    Add array of questions and choices

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+const questions_Options = [
+    {
+        ques: "What is the biggest capacity cricket ground in England?",
+        opts: ["The Oval", "Rose Bowl", "Lords", "Old Trafford"],
+        correct_option: "Lords",
+    },
+    {
+        ques: "Who is the highest run-scorer of all time in International cricket?",
+        opts: ["Ricky Pointing", "Virat Kohli", "Keith Fletcher", "Sachin Tendulkar"],
+        correct_option: "Sachin Tendulkar",
+    },
+    {
+        ques: " Who did England beat in the final of the 2019 Cricket World Cup? ",
+        opts: ["New Zealand", "Australia", "India", "Japan "],
+        correct_option: "New Zealand",
+    },
+    {
+        ques: "Where will the 2023 Cricket World Cup be hosted?",
+        opts: ["Pakistan", "India", "England", "Ireland "],
+        correct_option: "India",
+    }
+    {
+        ques: "What is the highest first innings score in test cricket?",
+        opts: ["903", "803", "1000", "602"],
+        correct_option: "903", 
+    }
+];
