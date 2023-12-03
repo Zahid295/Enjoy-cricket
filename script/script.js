77b8c9948a4eaef5911f233753f7e53efff94c86
Zahid295 <>
2023-12-03 20:04:00 +0000

    Remove unnecessary testAnswer function call

--- script/script.js
+++ script/script.js
@@ -2,32 +2,31 @@
 
 /*jshint esversion: 6 */
 
-const questions_Options = [
-    {
-        ques: "What is the biggest capacity cricket ground in England?",
-        opts: ["The Oval", "Rose Bowl", "Lords", "Old Trafford"],
-        correct_option: "Lords",
-    },
-    {
-        ques: "Who is the highest run-scorer of all time in International cricket?",
-        opts: ["Ricky Pointing", "Virat Kohli", "Keith Fletcher", "Sachin Tendulkar"],
-        correct_option: "Sachin Tendulkar",
-    },
-    {
-        ques: " Who did England beat in the final of the 2019 Cricket World Cup? ",
-        opts: ["New Zealand", "Australia", "India", "Japan "],
-        correct_option: "New Zealand",
-    },
-    {
-        ques: "Where will the 2023 Cricket World Cup be hosted?",
-        opts: ["Pakistan", "India", "England", "Ireland "],
-        correct_option: "India",
-    },
-    {
-        ques: "What is the highest first innings score in test cricket?",
-        opts: ["903", "803", "1000", "602"],
-        correct_option: "903",
-    }
+const questions_Options = [{
+    ques: "What is the biggest capacity cricket ground in England?",
+    opts: ["The Oval", "Rose Bowl", "Lords", "Old Trafford"],
+    correct_option: "Lords",
+},
+{
+    ques: "Who is the highest run-scorer of all time in International cricket?",
+    opts: ["Ricky Pointing", "Virat Kohli", "Keith Fletcher", "Sachin Tendulkar"],
+    correct_option: "Sachin Tendulkar",
+},
+{
+    ques: " Who did England beat in the final of the 2019 Cricket World Cup? ",
+    opts: ["New Zealand", "Australia", "India", "Japan "],
+    correct_option: "New Zealand",
+},
+{
+    ques: "Where will the 2023 Cricket World Cup be hosted?",
+    opts: ["Pakistan", "India", "England", "Ireland "],
+    correct_option: "India",
+},
+{
+    ques: "What is the highest first innings score in test cricket?",
+    opts: ["903", "803", "1000", "602"],
+    correct_option: "903",
+}
 ];
 
 // Retrieving html elements in variables
@@ -112,11 +111,9 @@ nextButton.addEventListener('click', function () {
 
         quizFinish = false;
         score = 0;
-    }
-    else {
+    } else {
         testAnswer();
     }
-    testAnswer();
 
 });
 
@@ -142,14 +139,3 @@ function displayQuestions() {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
