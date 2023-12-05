dd59d3f1ee549087331b65c8b2e97d4a6810b19a
Zahid295 <>
2023-12-05 22:40:58 +0000

    Add new file to separate array of objects

---
+++ script/cricketObjects.js
@@ -0,0 +1,32 @@
+//Export Array for questions and choices
+export const questions_Options = [{
+    // For the first question and chices
+    ques: "What is the biggest capacity cricket ground in England?",
+    opts: ["The Oval", "Rose Bowl", "Lords", "Old Trafford"],
+    correct_option: "Lords",
+},
+{
+    // For the second question and choices
+    ques: "Who is the highest run-scorer of all time in International cricket?",
+    opts: ["Ricky Pointing", "Virat Kohli", "Keith Fletcher", "Sachin Tendulkar"],
+    correct_option: "Sachin Tendulkar",
+},
+{
+    // For the third question and choices
+    ques: " Who did England beat in the final of the 2019 Cricket World Cup? ",
+    opts: ["New Zealand", "Australia", "India", "Japan "],
+    correct_option: "New Zealand",
+},
+{
+    // For the fourth question and choices
+    ques: "Where will the 2023 Cricket World Cup be hosted?",
+    opts: ["Pakistan", "India", "England", "Ireland "],
+    correct_option: "India",
+},
+{
+    // For the fifth question and choices
+    ques: "What is the highest first innings score in test cricket?",
+    opts: ["903", "803", "1000", "602"],
+    correct_option: "903",
+}
+];
--- script/script.js
+++ script/script.js
@@ -1,33 +1,7 @@
-//Array for questions and choices
-
 /*jshint esversion: 6 */
 
-const questions_Options = [{
-    ques: "What is the biggest capacity cricket ground in England?",
-    opts: ["The Oval", "Rose Bowl", "Lords", "Old Trafford"],
-    correct_option: "Lords",
-},
-{
-    ques: "Who is the highest run-scorer of all time in International cricket?",
-    opts: ["Ricky Pointing", "Virat Kohli", "Keith Fletcher", "Sachin Tendulkar"],
-    correct_option: "Sachin Tendulkar",
-},
-{
-    ques: " Who did England beat in the final of the 2019 Cricket World Cup? ",
-    opts: ["New Zealand", "Australia", "India", "Japan "],
-    correct_option: "New Zealand",
-},
-{
-    ques: "Where will the 2023 Cricket World Cup be hosted?",
-    opts: ["Pakistan", "India", "England", "Ireland "],
-    correct_option: "India",
-},
-{
-    ques: "What is the highest first innings score in test cricket?",
-    opts: ["903", "803", "1000", "602"],
-    correct_option: "903",
-}
-];
+// Import Array for questions and choices from questions_Options.js
+import { questions_Options } from './cricketObjects.js'
 
 // Retrieving html elements in variables
 const quizBox = document.getElementById("main-box");
